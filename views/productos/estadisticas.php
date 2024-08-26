a27f8a503e6bd8469a0fc3030f30080174cfa64c
Roger <ronguerra51@gmail.com>
2024-08-26 09:08:24 -0600

    funcion js

--- views/productos/estadisticas.php
+++ views/productos/estadisticas.php
@@ -2,5 +2,17 @@
 
 <h3 class='class text-center'>Grafica de ventas</h3>
 
+<div class = 'row'>
+    <div class='col'>
+        <button type='button' class='btn btn-info w-100' id='actualizar'>Refresh</button>
+    </div>
+</div>
+
+<div class='row justify-content-center'>
+    <div class='col'>
+        <canvas id="chartVentas" width='100%'></canvas>
+    </div>
+</div>
+
 
 <script src="<?= asset('./build/js/productos/estadisticas.js') ?>"></script>
